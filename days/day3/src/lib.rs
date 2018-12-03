eac575a85e428ad1e9fee53b30a6fedda186cc42
Piotr Gabrielski <ppitpo@gmail.com>
2018-12-03 14:14:58 +0100

    Day 3: Reduce part B time complexity

--- days/day3/src/lib.rs
+++ days/day3/src/lib.rs
@@ -55,42 +55,27 @@ impl Day for Day3 {
         result.to_string()
     }
     fn get_part_b_result(&self) -> String {
-        let mut input = self.parse_input();
-        let mut array: Vec<Vec<usize>> = Vec::new();
-        let mut result = 0;
-        // Due to borrowing issues, it has to look this way... for now at least
-        for x in 0..input.len() {
-            for i in 0..(input[x][2] + input[x][4]) {
-                if i >= array.len() && input[x][2] + input[x][4] >= array.len() {
-                    array.push(Vec::new())
-                }
-                if i >= input[x][2] {
-                    for j in 0..(input[x][1] + input[x][3]) {
-                        if j >= array[i].len() && input[x][1] + input[x][3] >= array[i].len() {
-                            array[i].push(0);
-                        }
-                    }
-                    for j in input[x][1]..(input[x][1] + input[x][3]) {
-                        if array[i][j] != 0 {
-                            input[x].push(array[i][j]);
-                            for k in 0..input.len() {
-                                if input[k][0] == array[i][j] {
-                                    let id = input[x][0];
-                                    input[k].push(id);
-                                }
-                            }
-                        }
-                        array[i][j] = input[x][0];
+        let input = self.parse_input();
+        for (i, claim) in input.iter().enumerate() {
+            let mut overlaps = false;
+            for (j, other_claim) in input.iter().enumerate() {
+                if (i != j) {
+                    let other_x_end = other_claim[1] + other_claim[3] - 1;
+                    let other_y_end = other_claim[2] + other_claim[4] - 1;
+                    let x_end = claim[1] + claim[3] - 1;
+                    let y_end = claim[2] + claim[4] - 1;
+                    let overlaps_in_x = x_end >= other_claim[1] && other_x_end >= claim[1];
+                    let overlaps_in_y = y_end >= other_claim[2] && other_y_end >= claim[2];
+                    if overlaps_in_x && overlaps_in_y {
+                        overlaps = true;
+                        break;
                     }
                 }
             }
-        }
-        for claim in input {
-            if claim.len() == 5 {
-                result = claim[0];
-                break;
+            if !overlaps {
+                return claim[0].to_string();
             }
         }
-        result.to_string()
+        String::new()
     }
 }
