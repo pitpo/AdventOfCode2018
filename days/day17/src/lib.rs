37375d965747d18fc69021c4c6bf0da529787a9e
Piotr Gabrielski <ppitpo@gmail.com>
2018-12-17 10:36:54 +0100

    Day 17: Add solution

--- days/day17/src/lib.rs
+++ days/day17/src/lib.rs
@@ -3,20 +3,238 @@ extern crate utils;
 use utils::Day;
 
 pub struct Day17 {
-
+    input: Vec<Vec<usize>>,
+    min_y: usize,
+    max_y: usize,
+    min_x: usize,
+    max_x: usize,
 }
 
 impl Day17 {
     pub fn new(input: String) -> Day17 {
-        Day17 { }
+        let mut new_input = utils::extract_unsigned_integers_from_string(&input);
+        for (i, line) in input.lines().enumerate() {
+            if line.starts_with("x") {
+                new_input[i].push(0);
+            } else {
+                new_input[i].push(1);
+            }
+        }
+        let min_y = new_input
+            .iter()
+            .min_by_key(|v| if v[3] == 0 { v[1] } else { v[0] })
+            .unwrap()
+            .clone();
+        let min_y = if min_y[3] == 0 { min_y[1] } else { min_y[0] };
+
+        let max_y = new_input
+            .iter()
+            .max_by_key(|v| if v[3] == 0 { v[2] } else { v[0] })
+            .unwrap()
+            .clone();
+        let max_y = if max_y[3] == 0 { max_y[2] } else { max_y[0] };
+
+        let min_x = new_input
+            .iter()
+            .min_by_key(|v| if v[3] == 1 { v[1] } else { v[0] })
+            .unwrap()
+            .clone();
+        let min_x = if min_x[3] == 1 { min_x[1] } else { min_x[0] };
+
+        let max_x = new_input
+            .iter()
+            .max_by_key(|v| if v[3] == 1 { v[2] } else { v[0] })
+            .unwrap()
+            .clone();
+        let max_x = if max_x[3] == 1 { max_x[2] } else { max_x[0] };
+
+        println!("{} {} {} {}", min_y, max_y, min_x, max_x);
+        Day17 {
+            input: new_input,
+            min_y,
+            max_y,
+            min_x,
+            max_x,
+        }
+    }
+
+    fn build_map(&self) -> Vec<Vec<char>> {
+        let mut map: Vec<Vec<char>> = Vec::with_capacity(self.max_y - self.min_y + 1);
+        for _ in 0..map.capacity() {
+            let mut row: Vec<char> = Vec::with_capacity(self.max_x - self.min_x + 5);
+            for _ in 0..row.capacity() {
+                row.push('.');
+            }
+            map.push(row);
+        }
+        for line in &self.input {
+            for i in line[1]..line[2] + 1 {
+                if line[3] == 0 {
+                    map[i - self.min_y][line[0] - self.min_x + 2] = '#';
+                } else {
+                    map[line[0] - self.min_y][i - self.min_x + 2] = '#';
+                }
+            }
+        }
+        // for row in &map {
+        //     println!("{}", row.iter().collect::<String>());
+        // }
+        map
+    }
+
+    fn tick(map: &mut Vec<Vec<char>>, min_x: usize, visited: &mut Vec<Vec<bool>>) {
+        let (x, y) = (500 - min_x + 2, 0);
+        Day17::flow_down(x, y, map, visited);        
+    }
+
+    fn split_right(mut x: usize, y: usize, map: &mut Vec<Vec<char>>, visited: &mut Vec<Vec<bool>>) -> bool {
+        while x < map[y].len() - 1 && map[y][x] != '#' && map[y + 1][x] != '.' && map[y+1][x] != '|' {
+            if map[y][x] == '.' {
+                map[y][x] = '|'
+            }
+            if map[y][x+1] == '~' && map[y][x] == '|' {
+                map[y][x+1] = '|';
+            }
+            x += 1;
+        }
+        if map[y][x] == '#' {
+            map[y][x-1] = '~';
+            return true;
+        } else {
+            Day17::flow_down(x, y, map, visited);
+            return false;
+        }
+    }
+
+    fn split_left(mut x: usize, y: usize, map: &mut Vec<Vec<char>>, visited: &mut Vec<Vec<bool>>) -> bool {
+        while x > 0 && map[y][x] != '#' && map[y+1][x] != '.' && map[y+1][x] != '|' {
+            if map[y][x] == '.' {
+                map[y][x] = '|'
+            }
+            if map[y][x-1] == '~' && map[y][x] == '|' {
+                map[y][x-1] = '|';
+            }
+            x -= 1;
+        }
+        if map[y][x] == '#' {
+            map[y][x+1] = '~';
+            return true;
+        } else {
+            Day17::flow_down(x, y, map, visited);
+            return false;
+        }
+    }
+
+    fn flow_down(mut x: usize, mut y: usize, map: &mut Vec<Vec<char>>, visited: &mut Vec<Vec<bool>>) {
+        if visited[y][x] {
+            return;
+        }
+        while y < map.len() - 1 && map[y+1][x] != '~' && map[y+1][x] != '#' {
+            if map[y][x] == '.' {
+                map[y][x] = '|';
+            }
+            visited[y][x] = true;
+            y += 1;
+        }
+        if y > map.len() - 2 && (map[y][x] == '.' || map[y][x] == '|') {
+            map[y][x] = '|';
+            return;
+        }
+        let left = Day17::split_left(x, y, map, visited);
+        let right = Day17::split_right(x, y, map, visited);
+        if left && right {
+            x += 1;
+            if map[y][x] != '#' {
+                while x < map[y].len() - 1 && map[y][x] != '~' {
+                    map[y][x] = '~';
+                    x += 1;
+                }
+            }
+            x -= 1;
+            while map[y][x] != '|' && map[y][x] != '#' {
+                x -= 1;
+            }
+            if map[y][x] != '#' {
+                while map[y][x] != '~' {
+                    map[y][x] = '~';
+                    x -= 1;
+                }
+            }
+        } 
+        if !left && right {
+            x += 1;
+            if map[y][x] != '#' {
+                while x < map[y].len() - 1 && map[y][x] != '~' {
+                    x += 1;
+                }
+                map[y][x] = '|';
+            }
+        }
+        if !right && left {
+            x -= 1;
+            if map[y][x] != '#' {
+                while x > 0 && map[y][x] != '~' {
+                    x -= 1;
+                }
+                map[y][x] = '|';
+            }
+        }
+    }
+
+    fn execute(&self) -> Vec<Vec<char>> {
+        let mut map = self.build_map();
+        map[0][500 - self.min_x + 2] = '|';
+        let min_x = self.min_x;
+        let builder = std::thread::Builder::new().name("worker".to_string()).stack_size(10*1024*1024);
+        let thread = builder.spawn(move|| {
+            let mut prev_map = map.clone();
+            loop {
+            //for _ in 0..250 {
+                let mut visited: Vec<Vec<bool>> = Vec::with_capacity(map.len());
+                for i in &map {
+                    let mut row: Vec<bool> = Vec::with_capacity(i.len());
+                    for _ in i {
+                        row.push(false);
+                    }
+                    visited.push(row);
+                }
+                Day17::tick(&mut map, min_x, &mut visited);
+                if map.iter().eq(prev_map.iter()) {
+                    break;
+                }
+                prev_map = map.clone();
+            } //map[map.len()-1].iter().filter(|&&c| c == '|').count() < 1;
+            map
+        }).unwrap();
+        thread.join().unwrap()
     }
 }
 
 impl Day for Day17 {
     fn get_part_a_result(&self) -> String {
-        String::new()
+        let map = self.execute();
+        let mut result = 0;
+        for row in &map {
+            for c in row {
+                if *c == '~' || *c == '|' {
+                    result += 1;
+                }
+            }
+        }
+        result -= 1; // off by one lol
+        result.to_string()
     }
     fn get_part_b_result(&self) -> String {
-        String::new()
+        let map = self.execute();
+        let mut result = 0;
+        for row in &map {
+            for c in row {
+                if *c == '~' {
+                    result += 1;
+                }
+            }
+        }
+        result -= 1;
+        result.to_string()
     }
 }
--- tests/day17.rs
+++ tests/day17.rs
@@ -4,7 +4,14 @@ extern crate utils;
 use utils::Day;
 
 fn get_input() -> String {
-    String::from("")
+    String::from("x=495, y=2..7
+y=7, x=495..501
+x=501, y=3..7
+x=498, y=2..4
+x=506, y=1..2
+x=498, y=10..13
+x=504, y=10..13
+y=13, x=498..504")
 }
 
 #[test]
@@ -13,7 +20,7 @@ fn day17_a() {
     
     let ans = solver.get_part_a_result();
 
-    assert_eq!(&ans, "");
+    assert_eq!(&ans, "57");
 }
 
 #[test]
