3084e3300ca830b67ecb39a530a4a7e89291f24b
Piotr Gabrielski <ppitpo@gmail.com>
2018-12-23 07:58:20 +0100

    Day 23: Add part A solution

--- days/day23/src/lib.rs
+++ days/day23/src/lib.rs
@@ -3,20 +3,48 @@ extern crate utils;
 use utils::Day;
 
 pub struct Day23 {
-
+    input: Vec<Vec<isize>>,
 }
 
 impl Day23 {
     pub fn new(input: String) -> Day23 {
-        Day23 { }
+        let input = utils::extract_integers_from_string(&input);
+        Day23 { input }
+    }
+
+    fn get_strongest_nanobot(&self) -> usize {
+        self.input.iter().enumerate().map(|(i, bot)| (i, bot[3])).max_by_key(|bot| bot.1).unwrap().clone().0 as usize
+    }
+
+    fn calculate_distances(&self, vec: &mut Vec<Vec<isize>>, strongest_bot: usize) {
+        let strongest_bot = vec[strongest_bot].clone();
+        for nanobot in vec {
+            let distance = (nanobot[0] - strongest_bot[0]).abs() + (nanobot[1] - strongest_bot[1]).abs() + (nanobot[2] - strongest_bot[2]).abs();
+            if nanobot.len() < 5 {
+                nanobot.push(distance);
+            } else {
+                nanobot[4] = distance;
+            }
+        }
     }
 }
 
 impl Day for Day23 {
     fn get_part_a_result(&self) -> String {
-        String::new()
+        let strongest_bot = self.get_strongest_nanobot();
+        let mut vec = self.input.clone();
+        self.calculate_distances(&mut vec, strongest_bot);
+        let strongest_distance = vec[strongest_bot][3];
+        let mut result = 0;
+        for nanobot in vec {
+            if nanobot[4] <= strongest_distance {
+                result += 1;
+            }
+        }
+        result.to_string()
     }
     fn get_part_b_result(&self) -> String {
+        // I give up
         String::new()
     }
 }
--- tests/day23.rs
+++ tests/day23.rs
@@ -4,21 +4,42 @@ extern crate utils;
 use utils::Day;
 
 fn get_input() -> String {
-    String::from("")
+    String::from("pos=<0,0,0>, r=4
+pos=<1,0,0>, r=1
+pos=<4,0,0>, r=3
+pos=<0,2,0>, r=1
+pos=<0,5,0>, r=3
+pos=<0,0,3>, r=1
+pos=<1,1,1>, r=1
+pos=<1,1,2>, r=1
+pos=<1,3,1>, r=1")
 }
 
 #[test]
 fn day23_a() {
-    let solver = day23::Day23::new(get_input());
+    let solver = day23::Day23::new(String::from("pos=<0,0,0>, r=4
+pos=<1,0,0>, r=1
+pos=<4,0,0>, r=3
+pos=<0,2,0>, r=1
+pos=<0,5,0>, r=3
+pos=<0,0,3>, r=1
+pos=<1,1,1>, r=1
+pos=<1,1,2>, r=1
+pos=<1,3,1>, r=1"));
     
     let ans = solver.get_part_a_result();
 
-    assert_eq!(&ans, "");
+    assert_eq!(&ans, "7");
 }
 
 #[test]
 fn day23_b() {
-    let solver = day23::Day23::new(get_input());
+    let solver = day23::Day23::new(String::from("pos=<10,12,12>, r=2
+pos=<12,14,12>, r=2
+pos=<16,12,12>, r=4
+pos=<14,14,14>, r=6
+pos=<50,50,50>, r=200
+pos=<10,10,10>, r=5"));
 
     let ans = solver.get_part_b_result();
     
