b9d0adcb1a856ee25a6a1449af9ca539e7c83410
Piotr Gabrielski <ppitpo@gmail.com>
2018-12-25 06:47:19 +0100

    Day 25: Add solution

--- days/day25/src/lib.rs
+++ days/day25/src/lib.rs
@@ -3,20 +3,69 @@ extern crate utils;
 use utils::Day;
 
 pub struct Day25 {
+    input: Vec<Vec<isize>>,
 }
 
 impl Day25 {
     pub fn new(input: String) -> Day25 {
-        Day25 { }
+        let input = utils::extract_integers_from_string(&input);
+        Day25 { input }
     }
 
+    fn is_adjacent(&self, lhs: &Vec<isize>, rhs: &Vec<isize>) -> bool {
+        (lhs[0] - rhs[0]).abs() + (lhs[1] - rhs[1]).abs() + (lhs[2] - rhs[2]).abs() + (lhs[3] - rhs[3]).abs() <= 3
+    }
 }
 
 impl Day for Day25 {
     fn get_part_a_result(&self) -> String {
-        String::new()
+        let mut constellations: Vec<Vec<Vec<isize>>> = Vec::new();
+        for coords in self.input.iter() {
+            let mut found = false;
+            for constellation in &mut constellations {
+                let mut can_add = false;
+                for other_coords in constellation.iter() {
+                    if self.is_adjacent(&coords, other_coords) {
+                        found = true;
+                        can_add = true;
+                    }
+                }
+                if can_add {
+                    constellation.push(coords.clone());
+                }
+            }
+            if !found {
+                let mut constellation: Vec<Vec<isize>> = Vec::new();
+                constellation.push(coords.clone());
+                constellations.push(constellation);
+            } else {
+                let mut i = 0;
+                while i < constellations.len() {
+                    let mut j = i + 1;
+                    while j < constellations.len() {
+                        let mut join = false;
+                        'outer: for coords in &constellations[i] {
+                            for other_coords in &constellations[j] {
+                                if self.is_adjacent(coords, other_coords) {
+                                    join = true;
+                                    break 'outer;
+                                }
+                            }
+                        }
+                        if join {
+                            constellations[i] = constellations[i].clone().into_iter().chain(constellations[j].clone().into_iter()).collect::<Vec<Vec<isize>>>();
+                            constellations.remove(j);
+                        } else {
+                            j += 1;
+                        }
+                    }
+                    i += 1;
+                }
+            }
+        }
+        constellations.len().to_string()
     }
     fn get_part_b_result(&self) -> String {
-        String::new()
+        String::from("Merry Christmas!")
     }
 }
--- tests/day25.rs
+++ tests/day25.rs
@@ -3,24 +3,81 @@ extern crate utils;
 
 use utils::Day;
 
-fn get_input() -> String {
-    String::from("")
-}
-
 #[test]
-fn day25_a() {
-    let solver = day25::Day25::new(get_input());
+fn day25_a1() {
+    let solver = day25::Day25::new(String::from("0,0,0,0
+ 3,0,0,0
+ 0,3,0,0
+ 0,0,3,0
+ 0,0,0,3
+ 0,0,0,6
+ 9,0,0,0
+12,0,0,0"));
     
     let ans = solver.get_part_a_result();
 
-    assert_eq!(&ans, "");
+    assert_eq!(&ans, "2");
 }
 
 #[test]
-fn day25_b() {
-    let solver = day25::Day25::new(get_input());
-
-    let ans = solver.get_part_b_result();
+fn day25_a2() {
+    let solver = day25::Day25::new(String::from("-1,2,2,0
+0,0,2,-2
+0,0,0,-2
+-1,2,0,0
+-2,-2,-2,2
+3,0,2,-1
+-1,3,2,2
+-1,0,-1,0
+0,2,1,-2
+3,0,0,0"));
     
-    assert_eq!(&ans, "");
+    let ans = solver.get_part_a_result();
+
+    assert_eq!(&ans, "4");
 }
+
+#[test]
+fn day25_a3() {
+    let solver = day25::Day25::new(String::from("1,-1,0,1
+2,0,-1,0
+3,2,-1,0
+0,0,3,1
+0,0,-1,-1
+2,3,-2,0
+-2,2,0,0
+2,-2,0,-1
+1,-1,0,-1
+3,2,0,2"));
+    
+    let ans = solver.get_part_a_result();
+
+    assert_eq!(&ans, "3");
+}
+
+#[test]
+fn day25_a4() {
+    let solver = day25::Day25::new(String::from("1,-1,-1,-2
+-2,-2,0,1
+0,2,1,3
+-2,3,-2,1
+0,2,3,-2
+-1,-1,1,-2
+0,-2,-1,0
+-2,2,3,-1
+1,2,2,0
+-1,-2,0,-2"));
+    
+    let ans = solver.get_part_a_result();
+
+    assert_eq!(&ans, "8");
+}
+
+// #[test]
+// fn day25_b() {
+//     let solver = day25::Day25::new(get_input());
+
+//     let ans = solver.get_part_b_result();
+    
+//     assert_eq!(&ans, "");
+// }
